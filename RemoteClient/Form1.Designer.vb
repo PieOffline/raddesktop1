22aeb0083f1c13bd9b141d72232a7fd517831306
Paimaan Hasmi <FITZ11134@fitzalan.cardiff.sch.uk>
2025-05-06 14:53:16 +0100

    Add project files.

---
+++ RemoteClient/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtIP = New System.Windows.Forms.TextBox()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.picRemote = New System.Windows.Forms.PictureBox()
+        CType(Me.picRemote, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtIP
+        '
+        Me.txtIP.Location = New System.Drawing.Point(12, 12)
+        Me.txtIP.Name = "txtIP"
+        Me.txtIP.Size = New System.Drawing.Size(100, 20)
+        Me.txtIP.TabIndex = 0
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(127, 5)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(99, 27)
+        Me.btnConnect.TabIndex = 1
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'picRemote
+        '
+        Me.picRemote.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.picRemote.Location = New System.Drawing.Point(0, 0)
+        Me.picRemote.Name = "picRemote"
+        Me.picRemote.Size = New System.Drawing.Size(800, 450)
+        Me.picRemote.TabIndex = 2
+        Me.picRemote.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.picRemote)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.txtIP)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picRemote, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtIP As TextBox
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents picRemote As PictureBox
+End Class
---
+++ RemoteClient/Form1.vb
@@ -0,0 +1,103 @@
+﻿Imports System.Net
+
+Imports System.Net.Sockets
+
+Imports System.IO
+
+Imports System.Threading
+
+Imports System.Drawing
+
+
+
+Public Class Form1
+
+    Private imgClient As TcpClient
+
+    Private ctrlClient As TcpClient
+
+
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        imgClient = New TcpClient(txtIP.Text, 9000)
+        ctrlClient = New TcpClient(txtIP.Text, 9001)
+
+        ThreadPool.QueueUserWorkItem(AddressOf ReceiveImages)
+
+        ' Hook up mouse events on the PictureBox
+        AddHandler picRemote.MouseMove, AddressOf PicRemote_MouseMove
+        AddHandler picRemote.MouseClick, AddressOf PicRemote_MouseClick
+    End Sub
+
+
+
+
+    Private Sub ReceiveImages(state As Object)
+
+        Dim ns = imgClient.GetStream()
+
+        Dim br As New BinaryReader(ns)
+
+        Try
+
+            While True
+
+                Dim len = IPAddress.NetworkToHostOrder(br.ReadInt32())
+
+                Dim data = br.ReadBytes(len)
+
+                Using ms As New MemoryStream(data)
+
+                    Dim bmp = CType(Bitmap.FromStream(ms), Bitmap)
+
+                    picRemote.Invoke(Sub() picRemote.Image = New Bitmap(bmp))
+
+                End Using
+
+            End While
+
+        Catch ex As Exception
+
+            ' disconnected
+
+        End Try
+
+    End Sub
+
+
+
+    Private Sub PicRemote_MouseMove(sender As Object, e As MouseEventArgs)
+
+        SendControl(1, e.X, e.Y)
+
+    End Sub
+
+
+
+    Private Sub PicRemote_MouseClick(sender As Object, e As MouseEventArgs)
+
+        SendControl(2, e.X, e.Y)
+
+    End Sub
+
+
+
+    Private Sub SendControl(evt As Byte, x As Integer, y As Integer)
+
+        If ctrlClient Is Nothing OrElse Not ctrlClient.Connected Then Return
+
+        Dim ns = ctrlClient.GetStream()
+
+        Dim bw As New BinaryWriter(ns)
+
+        bw.Write(evt)
+
+        bw.Write(IPAddress.HostToNetworkOrder(x))
+
+        bw.Write(IPAddress.HostToNetworkOrder(y))
+
+        bw.Flush()
+
+    End Sub
+
+End Class
